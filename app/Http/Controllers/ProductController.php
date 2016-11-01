48f4a7c23e2e8b30c110ff30ea9ed2cf8ec665a9
Nguyễn Thanh <nguyenthanh@techmasters-Mac-mini-6.local>
2016-11-01 17:40:57 +0700

    Lecture 76 - Errors getClientOriginalName()

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProductRequest;
 
 use App\Cate;
-
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -16,7 +16,27 @@ class ProductController extends Controller
     }
 
     public function postAdd(ProductRequest $request){
-        dd($request->all());
+        // dd($request->all());
+        $file_name = $request->file('fImages')->getClientOriginalName();
+        $product = new Product;
+        $product->name = $request->txtName;
+        $product->alias = $request->txtName;
+        $product->price = $request->txtPrice;
+        $product->intro = $request->txtIntro;
+        $product->content = $request->txtContent;
+        $product->image = $file_name;
+        $product->keywords = $request->txtKeyword;
+        $product->description = $request->txtDescription;
+        $product->user_id = 1;
+        $product->cate_id = $request->txtCP;
+        // move images to uploads
+        $desPath = public_path().'resources/uploads';
+        $request->file('fImages')->move($desPath,$file_name);
+        $product->save();
+        return view('admin.product.product_list')->with([
+            'flash_message' => 'Bạn đã thêm thành công 1 sản phẩm mới',
+            'flash_level' => 'success'
+        ]);
     }
 
     public function getList(){
--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+
 class ProductRequest extends FormRequest
 {
     /**
@@ -24,13 +25,11 @@ class ProductRequest extends FormRequest
     public function rules()
     {
         return [
-            'txtCP' => 'required',
             'txtName' => 'required|unique:products,name',
             'txtPrice' => 'required',
             'txtIntro' => 'required',
             'txtContent' => 'required',
             'fImages' => 'required',
-            'txtOrder' => 'required',
             'txtDescription' => 'required'
         ];
     }
@@ -38,14 +37,11 @@ class ProductRequest extends FormRequest
     public function messages()
     {
         return [
-
-            'txtCP.required' => 'Bạn bắt buộc phải nhập Parent ID ',
             'txtName.required' => 'Bạn bắt buộc phải nhập Product Name ',
             'txtPrice.required' => 'Bạn bắt buộc phải nhập Product Price ',
             'txtIntro.required' => 'Bạn bắt buộc phải nhập Product Intro ',
             'txtContent.required' => 'Bạn bắt buộc phải nhập Product Content ',
             'fImages.required' => 'Bạn bắt buộc phải nhập Product Images ',
-            'txtOrder.required' => 'Bạn bắt buộc phải nhập Product Order ',
             'txtDescription.required' => 'Bạn bắt buộc phải nhập Product Description ',
             'txtName.unique' => 'Tên của sản phẩm đã tồn tại , mời bạn đặt tên khác cho sản phẩm'
         ];
