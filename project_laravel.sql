4eade6aa7ffcac6a313f18d77c19e1630c93e9dc
AkKeRise <akkerise@gmail.com>
2016-12-12 21:45:26 +0700

    Change location work device

--- project_laravel.sql
+++ project_laravel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 27, 2016 at 12:58 PM
+-- Generation Time: Dec 12, 2016 at 03:44 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -116,7 +116,6 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `name`, `alias`, `price`, `intro`, `content`, `image`, `keywords`, `description`, `user_id`, `cate_id`, `created_at`, `updated_at`) VALUES
 (13, 'Áo Măng tô', 'Áo Măng tô', 88888888, 'Áo Măng tô giẻ rách\r\n', '<p>&Aacute;o Măng t&ocirc; giẻ r&aacute;ch</p>\r\n', 'ao-so-mi-hollister-mau-den.png', 'ao-mang-to', '                áo măng tô hiệu giẻ rách\r\n                ', 7, 2, '2016-11-14 00:27:06', '2016-11-27 01:22:34'),
-(14, 'Áo Sơ Mi Công Cộng', 'Áo Sơ Mi Công Cộng', 1111111, 'Áo Sơ Mi Công Cộng', '<p>&Aacute;o Sơ Mi C&ocirc;ng Cộng</p>\r\n', 'macos_sierra_new-wallpaper-2560x1600.jpg', 'ao-so-mi', 'Áo Sơ Mi Công Cộng', 1, 2, '2016-11-15 19:46:36', '2016-11-17 01:50:13'),
 (15, 'Quần Bóng', 'Quần Bóng', 99999999, '<p>Quần B&oacute;ng</p>\r\n', 'Quần Bóng', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'quan-bong', 'Quần Bóng', 1, 1, '2016-11-15 19:49:05', '2016-11-15 19:49:05'),
 (16, 'Quần Vải', 'Quần Vải', 989898, '<p>Quần Vải Osen</p>\r\n', 'Quần Vải Osen', 'ao-thun-nam-anti-x-mau-xanh-nhat--co-co-ipxi4907e0w0h0.jpg', 'quan-vai-osen', 'Quần Vải Osen', 7, 1, '2016-11-20 21:49:22', '2016-11-20 21:49:22'),
 (17, 'Quần Jean', 'Quần Jean', 1000000, '<p>Quần Jean</p>\r\n', 'Quần Jean Xuất Khẩu', 'enbac.jpg', 'quan-jean', 'Quần Jean Xuất Khẩu', 7, 1, '2016-11-26 23:06:07', '2016-11-26 23:06:07'),
@@ -124,7 +123,37 @@ INSERT INTO `products` (`id`, `name`, `alias`, `price`, `intro`, `content`, `ima
 (19, 'Tất Ý', 'Tất Ý', 10998982, '<p>Tất nhập khẩu từ &Yacute;</p>\r\n', 'Tất nhập khẩu từ Ý', 'tat1.jpg', 'tat-nhap-khau', 'Tất nhập khẩu từ Ý', 7, 4, '2016-11-26 23:11:22', '2016-11-26 23:11:22'),
 (20, 'Giầy Ý', 'Giầy Ý', 198000, '<p>Giầy Nhập Khẩu Từ &Yacute;</p>\r\n', 'Giầy Nhập Khẩu Từ Ý', 'giay1.jpg', 'giay-nhap-khau-tu-y', 'Giầy Nhập Khẩu Từ Ý', 7, 5, '2016-11-26 23:13:47', '2016-11-26 23:13:47'),
 (21, 'Tủ Lạnh', 'Tủ Lạnh', 8888888, '<p>Tủ lạnh nhập khẩu</p>\r\n', 'Tủ lạnh nhập khẩu', 'tu-lanh1.jpg', 'tu-lanh', 'Tủ lạnh nhập khẩu', 7, 7, '2016-11-27 00:19:23', '2016-11-27 00:19:23'),
-(22, 'Quạt Điện Cơ', 'Quạt Điện Cơ', 1313131, '<p>Quạt Điện Cơ</p>\r\n', 'Quạt Điện Cơ', 'quat1.jpg', 'quat-dien-co', 'Quạt Điện Cơ', 7, 8, '2016-11-27 00:21:48', '2016-11-27 00:21:48');
+(22, 'Quạt Điện Cơ', 'Quạt Điện Cơ', 1313131, '<p>Quạt Điện Cơ</p>\r\n', 'Quạt Điện Cơ', 'quat1.jpg', 'quat-dien-co', 'Quạt Điện Cơ', 7, 8, '2016-11-27 00:21:48', '2016-11-27 00:21:48'),
+(23, 'Quần Kaki Nam', 'Quần Kaki Nam', 250000, 'Quần Kaki', 'Quần Kaki', 'quan-kaki-nam.jpg', 'quan-kaki-nam', 'Quần Kaki', 7, 1, '2016-11-27 19:44:34', '2016-11-27 19:44:34'),
+(24, 'Desktop Setup One', 'Desktop Setup One', 12000000, 'Desktop Setup One', 'Desktop Setup One', 'desktop1.jpg', 'desktop-setup-one', 'Desktop Setup One', 7, 9, '2016-11-27 20:13:26', '2016-11-27 20:13:26'),
+(36, 'Apple', 'Apple', 84574857, '<p>Apple</p>\r\n', 'Apple', '107105253-apple-tech-xlarge_trans++bFoXn4d1uVNGRa1pDHVYNFbFJiGQSGUwQFXFdwSXZiw.jpg', 'apple', 'apple', 7, 3, '2016-11-28 01:08:44', '2016-11-28 01:08:44'),
+(37, 'ewfewfềw', 'ewfewfềw', 3434, '<p>wềw</p>\r\n', 'ưefwef', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'regre', 'egreẻger', 7, 4, '2016-11-28 01:21:30', '2016-11-28 01:21:30'),
+(38, 'ihkjhdsfed', 'ihkjhdsfed', 232323, '<p>ihkjhdsfe</p>\r\n', 'ưefwef', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'regref', 'ưefwefef', 7, 4, '2016-11-28 01:25:17', '2016-11-28 01:25:17'),
+(39, 'ihkjhdsfedưd', 'ihkjhdsfedưd', 232323, '<p>qưd</p>\r\n', 'qưd', 'dep3.jpg', 'regref', 'qưd', 7, 6, '2016-11-28 01:26:00', '2016-11-28 01:26:00'),
+(40, 'qưdihkjhdsfedưdưddsf', 'qưdihkjhdsfedưdưddsf', 234234, '<p>ưdqưd</p>\r\n', 'qđưqưd', 'ao-so-mi-nu-2.jpg', 'ưerwer', 'qưdqwd', 7, 6, '2016-11-28 01:29:50', '2016-11-28 01:29:50'),
+(41, 'qưdihkjhdsfedưdưddsfewf', 'qưdihkjhdsfedưdưddsfewf', 234234, '<p>ewfưefewf</p>\r\n', 'ưefewfưefwef', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'ưerwerưef', 'ưefwefè', 7, 3, '2016-11-28 01:33:48', '2016-11-28 01:33:48'),
+(42, 'qưdihkjhdsfedưdưddsfewfqưd', 'qưdihkjhdsfedưdưddsfewfqưd', 234234, '<p>ưef</p>\r\n', 'ưefưef', 'chuyen-si-le-quan-kaki-nam-facioshop-qa164.jpg', 'ưerwerưefưef', 'ưefưef', 7, 2, '2016-11-28 01:34:25', '2016-11-28 01:34:25'),
+(43, 'qưdihkjhdsfedưdưddsfewfqưdwef', 'qưdihkjhdsfedưdưddsfewfqưdwef', 234234, '<p>ưef</p>\r\n', 'ưefưef', 'ao-so-mi-nu-1.jpg', 'ưerwerưefưefưef', 'ưefưef', 7, 2, '2016-11-28 01:35:22', '2016-11-28 01:35:22'),
+(44, 'qưdihkjhdsfedưdưddsfewfqưdwefưef', 'qưdihkjhdsfedưdưddsfewfqưdwefưef', 2342343, '<p>ưef</p>\r\n', 'ưefưefewf', 'dep1.jpg', 'ưerwerưefưefưefưefew', 'ưefwef', 7, 3, '2016-11-28 01:36:19', '2016-11-28 01:36:19'),
+(45, 'excite', 'excite', 45473525, '<p>asdasdaksldj</p>\r\n', 'jdashfjdshfkj', 'ao-so-mi-nu-2.jpg', 'asdiasudiaus', 'asuaifuioaufia', 7, 3, '2016-11-28 01:52:42', '2016-11-28 01:52:42'),
+(46, 'Nước Hồn', 'Nước Hồn', 47534823, '<p>nước hồn</p>\r\n', 'nước hồn', 'ao-so-mi-nu-2.jpg', 'nuoc-hon', 'Nước Hồn', 7, 3, '2016-11-28 02:17:59', '2016-11-28 02:17:59'),
+(47, 'Quần Lin', 'Quần Lin', 42342342, '<p>Quần Lin</p>\r\n', 'Quần Lin', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'quan-lin', 'Quần Lin', 7, 1, '2016-11-28 02:59:27', '2016-11-28 02:59:27'),
+(48, 'Nồi Cơm Điện', 'Nồi Cơm Điện', 83439483, '<p>Nồi Cơm Điện</p>\r\n', 'Nồi Cơm Điện', '103733_noi-com-dien-panasonic-sr-tr184sra-3.jpg', 'noi-com-dien', 'Nồi Cơm Điện', 7, 7, '2016-11-28 03:11:38', '2016-11-28 03:11:38'),
+(49, 'Nồi Cơm Điện Cooku', 'Nồi Cơm Điện Cooku', 38473287, '<p>Nồi Cơm Điện Cooku</p>\r\n', 'Nồi Cơm Điện Cooku', 'noi-com-dien-cuckoo-cr_1713r_3.jpg', 'noi-com-dien-cooku', 'Nồi Cơm Điện Cooku', 7, 7, '2016-11-28 03:22:25', '2016-11-28 03:22:25'),
+(50, 'TV 32inch', 'TV 32inch', 352820598, '<p>Tivi 32 inch</p>\r\n', 'Tivi 32 inch', 'tv1.jpg', 'Tivi 32 inch', 'Tivi 32 inch', 7, 7, '2016-11-28 03:29:25', '2016-11-28 03:29:25'),
+(51, 'Quần Vẩy', 'Quần Vẩy', 48548875, '<p>Quần Vẩy</p>\r\n', 'Quần Vẩy', 'quanvay1.jpg', 'Quần Vẩy', 'Quần Vẩy', 7, 1, '2016-11-28 03:58:55', '2016-11-28 03:58:55'),
+(52, 'Quần Lởm', 'Quần Lởm', 32752358, '<p><span style="color:rgb(17, 17, 17); font-family:ubuntu,arial,libra sans,sans-serif; font-size:15px">Quần Lởm</span></p>\r\n', 'Quần Lởm', 'quanvay1.jpg', 'quan-lom', 'Quần Lởm', 7, 1, '2016-11-29 19:37:41', '2016-11-29 19:37:41'),
+(53, 'Quần Jogger', 'Quần Jogger', 4854348, '<p>Quần Jogger</p>\r\n', 'Quần Jogger', 'quan-jogger1.jpg', 'quan-jooger', 'Quần Jogger', 7, 1, '2016-11-29 20:27:44', '2016-11-29 20:27:44'),
+(54, 'Quần Culottes', 'Quần Culottes', 58457, '<p>Quần Culottes</p>\r\n', 'Quần Culottes', '9192aeb89a4f1de8f1764ffe3d85edb9.jpg', 'Quần Culottes', 'Quần Culottes', 7, 1, '2016-11-30 00:13:10', '2016-11-30 00:13:10'),
+(55, 'Bugatti', 'Bugatti', 182534683, '<p>Bugatti</p>\r\n', 'Bugatti', 'bugatti1.jpg', 'bugatti', 'Bugatti', 7, 7, '2016-11-30 00:36:42', '2016-11-30 00:36:42'),
+(56, 'Bugatti 2', 'Bugatti 2', 18253432, '<p>Bugatti 2</p>\r\n', 'Bugatti 2', 'bugatti2.jpg', 'bugatti 2', 'Bugatti 2', 7, 7, '2016-11-30 01:01:37', '2016-11-30 01:01:37'),
+(57, 'Bugatti 3', 'Bugatti 3', 18253432, '<p>Bugatti 3</p>\r\n', 'Bugatti 3', 'bugatti3.jpg', 'bugatti 3', 'Bugatti 3', 7, 7, '2016-11-30 01:06:12', '2016-11-30 01:06:12'),
+(58, 'Bugatti 4', 'Bugatti 4', 182534322, '<p>Bugatti 4</p>\r\n', 'Bugatti 4', 'bugatti4.jpg', 'bugatti 3', 'Bugatti 4', 7, 7, '2016-11-30 01:27:47', '2016-11-30 01:27:47'),
+(59, 'Bugatti 6', 'Bugatti 6', 45343636, '<p>Bugatti 6</p>\r\n', 'Bugatti 6', 'bugatti6.jpg', 'Bugatti 6', 'Bugatti 6', 7, 7, '2016-11-30 01:58:38', '2016-11-30 01:58:38'),
+(60, 'Bugatti 7', 'Bugatti 7', 453436326, '<p>Bugatti 7</p>\r\n', 'Bugatti 7', 'bugatti7.jpg', 'Bugatti 7', 'Bugatti 7', 7, 7, '2016-11-30 02:01:23', '2016-11-30 02:01:23'),
+(61, 'Bugatti 8', 'Bugatti 8', 82357285, '<p>Bugatti 8</p>\r\n', 'Bugatti 8', 'bugatti8.jpg', 'Bugatti 8', 'Bugatti 8', 7, 7, '2016-11-30 02:04:09', '2016-11-30 02:04:09'),
+(62, 'Iphone 6', 'Iphone 6', 34538843, '<p>Iphone 6</p>\r\n', 'Iphone 6', 'apple_iphone6_hero_cc003.jpg', 'Iphone 6', 'Iphone 6', 7, 7, '2016-11-30 02:29:11', '2016-11-30 02:29:11'),
+(63, 'Iphone 6S', 'Iphone 6S', 342423423, '<p>Iphone 6S</p>\r\n', 'Iphone 6S', 'apple_iphone6_hero_cc003.jpg', 'Iphone 6', 'Iphone 6S', 7, 7, '2016-11-30 02:35:10', '2016-11-30 02:35:10');
 
 -- --------------------------------------------------------
 
@@ -145,17 +174,9 @@ CREATE TABLE `product_images` (
 --
 
 INSERT INTO `product_images` (`id`, `image`, `product_id`, `created_at`, `updated_at`) VALUES
-(37, 'ao-so-mi-hollister-mau-den.png', 14, NULL, NULL),
-(38, 'ao-so-mi-hollister-mau-den.png', 14, NULL, NULL),
-(39, 'ao-so-mi-hollister-mau-den.png', 14, NULL, NULL),
-(40, 'ao-so-mi-hollister-mau-den.png', 14, NULL, NULL),
-(41, 'ao-so-mi-hollister-mau-den.png', 14, NULL, NULL),
 (42, '2016-China-Supplier-8-Colors-Long-Style.jpg', 15, NULL, NULL),
 (43, '2016-China-Supplier-8-Colors-Long-Style.jpg', 15, NULL, NULL),
 (44, '2016-China-Supplier-8-Colors-Long-Style.jpg', 15, NULL, NULL),
-(46, 'maxresdefault.jpg', 14, NULL, NULL),
-(47, 'macos_sierra_new-wallpaper-2560x1600.jpg', 14, NULL, NULL),
-(48, '1UAT13OZT1BZ.jpg', 14, NULL, NULL),
 (49, 'quan-jean-1.jpg', 17, NULL, NULL),
 (50, 'quan-jean-2.jpeg', 17, NULL, NULL),
 (51, 'ao-so-mi-nu-1.jpg', 18, NULL, NULL),
@@ -172,7 +193,8 @@ INSERT INTO `product_images` (`id`, `image`, `product_id`, `created_at`, `update
 (62, 'tu-lanh-2.jpg', 21, NULL, NULL),
 (63, 'quat1.jpg', 22, NULL, NULL),
 (64, 'quat2.jpg', 22, NULL, NULL),
-(65, 'quat3.jpg', 22, NULL, NULL);
+(65, 'quat3.jpg', 22, NULL, NULL),
+(66, 'apple-iphone-6-1.jpg', 62, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -274,12 +296,12 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 --
 -- AUTO_INCREMENT for table `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 --
 -- AUTO_INCREMENT for table `users`
 --
