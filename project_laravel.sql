5289ecf6a677bb03f4ae00be18ff783b78fae01d
AkKeRise <akkerise@gmail.com>
2016-12-21 19:48:15 +0700

    Add thehientk

--- project_laravel.sql
+++ project_laravel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 12, 2016 at 03:44 PM
+-- Generation Time: Dec 21, 2016 at 01:47 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -127,14 +127,6 @@ INSERT INTO `products` (`id`, `name`, `alias`, `price`, `intro`, `content`, `ima
 (23, 'Quần Kaki Nam', 'Quần Kaki Nam', 250000, 'Quần Kaki', 'Quần Kaki', 'quan-kaki-nam.jpg', 'quan-kaki-nam', 'Quần Kaki', 7, 1, '2016-11-27 19:44:34', '2016-11-27 19:44:34'),
 (24, 'Desktop Setup One', 'Desktop Setup One', 12000000, 'Desktop Setup One', 'Desktop Setup One', 'desktop1.jpg', 'desktop-setup-one', 'Desktop Setup One', 7, 9, '2016-11-27 20:13:26', '2016-11-27 20:13:26'),
 (36, 'Apple', 'Apple', 84574857, '<p>Apple</p>\r\n', 'Apple', '107105253-apple-tech-xlarge_trans++bFoXn4d1uVNGRa1pDHVYNFbFJiGQSGUwQFXFdwSXZiw.jpg', 'apple', 'apple', 7, 3, '2016-11-28 01:08:44', '2016-11-28 01:08:44'),
-(37, 'ewfewfềw', 'ewfewfềw', 3434, '<p>wềw</p>\r\n', 'ưefwef', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'regre', 'egreẻger', 7, 4, '2016-11-28 01:21:30', '2016-11-28 01:21:30'),
-(38, 'ihkjhdsfed', 'ihkjhdsfed', 232323, '<p>ihkjhdsfe</p>\r\n', 'ưefwef', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'regref', 'ưefwefef', 7, 4, '2016-11-28 01:25:17', '2016-11-28 01:25:17'),
-(39, 'ihkjhdsfedưd', 'ihkjhdsfedưd', 232323, '<p>qưd</p>\r\n', 'qưd', 'dep3.jpg', 'regref', 'qưd', 7, 6, '2016-11-28 01:26:00', '2016-11-28 01:26:00'),
-(40, 'qưdihkjhdsfedưdưddsf', 'qưdihkjhdsfedưdưddsf', 234234, '<p>ưdqưd</p>\r\n', 'qđưqưd', 'ao-so-mi-nu-2.jpg', 'ưerwer', 'qưdqwd', 7, 6, '2016-11-28 01:29:50', '2016-11-28 01:29:50'),
-(41, 'qưdihkjhdsfedưdưddsfewf', 'qưdihkjhdsfedưdưddsfewf', 234234, '<p>ewfưefewf</p>\r\n', 'ưefewfưefwef', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'ưerwerưef', 'ưefwefè', 7, 3, '2016-11-28 01:33:48', '2016-11-28 01:33:48'),
-(42, 'qưdihkjhdsfedưdưddsfewfqưd', 'qưdihkjhdsfedưdưddsfewfqưd', 234234, '<p>ưef</p>\r\n', 'ưefưef', 'chuyen-si-le-quan-kaki-nam-facioshop-qa164.jpg', 'ưerwerưefưef', 'ưefưef', 7, 2, '2016-11-28 01:34:25', '2016-11-28 01:34:25'),
-(43, 'qưdihkjhdsfedưdưddsfewfqưdwef', 'qưdihkjhdsfedưdưddsfewfqưdwef', 234234, '<p>ưef</p>\r\n', 'ưefưef', 'ao-so-mi-nu-1.jpg', 'ưerwerưefưefưef', 'ưefưef', 7, 2, '2016-11-28 01:35:22', '2016-11-28 01:35:22'),
-(44, 'qưdihkjhdsfedưdưddsfewfqưdwefưef', 'qưdihkjhdsfedưdưddsfewfqưdwefưef', 2342343, '<p>ưef</p>\r\n', 'ưefưefewf', 'dep1.jpg', 'ưerwerưefưefưefưefew', 'ưefwef', 7, 3, '2016-11-28 01:36:19', '2016-11-28 01:36:19'),
 (45, 'excite', 'excite', 45473525, '<p>asdasdaksldj</p>\r\n', 'jdashfjdshfkj', 'ao-so-mi-nu-2.jpg', 'asdiasudiaus', 'asuaifuioaufia', 7, 3, '2016-11-28 01:52:42', '2016-11-28 01:52:42'),
 (46, 'Nước Hồn', 'Nước Hồn', 47534823, '<p>nước hồn</p>\r\n', 'nước hồn', 'ao-so-mi-nu-2.jpg', 'nuoc-hon', 'Nước Hồn', 7, 3, '2016-11-28 02:17:59', '2016-11-28 02:17:59'),
 (47, 'Quần Lin', 'Quần Lin', 42342342, '<p>Quần Lin</p>\r\n', 'Quần Lin', '2016-China-Supplier-8-Colors-Long-Style.jpg', 'quan-lin', 'Quần Lin', 7, 1, '2016-11-28 02:59:27', '2016-11-28 02:59:27'),
@@ -145,15 +137,19 @@ INSERT INTO `products` (`id`, `name`, `alias`, `price`, `intro`, `content`, `ima
 (52, 'Quần Lởm', 'Quần Lởm', 32752358, '<p><span style="color:rgb(17, 17, 17); font-family:ubuntu,arial,libra sans,sans-serif; font-size:15px">Quần Lởm</span></p>\r\n', 'Quần Lởm', 'quanvay1.jpg', 'quan-lom', 'Quần Lởm', 7, 1, '2016-11-29 19:37:41', '2016-11-29 19:37:41'),
 (53, 'Quần Jogger', 'Quần Jogger', 4854348, '<p>Quần Jogger</p>\r\n', 'Quần Jogger', 'quan-jogger1.jpg', 'quan-jooger', 'Quần Jogger', 7, 1, '2016-11-29 20:27:44', '2016-11-29 20:27:44'),
 (54, 'Quần Culottes', 'Quần Culottes', 58457, '<p>Quần Culottes</p>\r\n', 'Quần Culottes', '9192aeb89a4f1de8f1764ffe3d85edb9.jpg', 'Quần Culottes', 'Quần Culottes', 7, 1, '2016-11-30 00:13:10', '2016-11-30 00:13:10'),
-(55, 'Bugatti', 'Bugatti', 182534683, '<p>Bugatti</p>\r\n', 'Bugatti', 'bugatti1.jpg', 'bugatti', 'Bugatti', 7, 7, '2016-11-30 00:36:42', '2016-11-30 00:36:42'),
-(56, 'Bugatti 2', 'Bugatti 2', 18253432, '<p>Bugatti 2</p>\r\n', 'Bugatti 2', 'bugatti2.jpg', 'bugatti 2', 'Bugatti 2', 7, 7, '2016-11-30 01:01:37', '2016-11-30 01:01:37'),
-(57, 'Bugatti 3', 'Bugatti 3', 18253432, '<p>Bugatti 3</p>\r\n', 'Bugatti 3', 'bugatti3.jpg', 'bugatti 3', 'Bugatti 3', 7, 7, '2016-11-30 01:06:12', '2016-11-30 01:06:12'),
 (58, 'Bugatti 4', 'Bugatti 4', 182534322, '<p>Bugatti 4</p>\r\n', 'Bugatti 4', 'bugatti4.jpg', 'bugatti 3', 'Bugatti 4', 7, 7, '2016-11-30 01:27:47', '2016-11-30 01:27:47'),
 (59, 'Bugatti 6', 'Bugatti 6', 45343636, '<p>Bugatti 6</p>\r\n', 'Bugatti 6', 'bugatti6.jpg', 'Bugatti 6', 'Bugatti 6', 7, 7, '2016-11-30 01:58:38', '2016-11-30 01:58:38'),
 (60, 'Bugatti 7', 'Bugatti 7', 453436326, '<p>Bugatti 7</p>\r\n', 'Bugatti 7', 'bugatti7.jpg', 'Bugatti 7', 'Bugatti 7', 7, 7, '2016-11-30 02:01:23', '2016-11-30 02:01:23'),
 (61, 'Bugatti 8', 'Bugatti 8', 82357285, '<p>Bugatti 8</p>\r\n', 'Bugatti 8', 'bugatti8.jpg', 'Bugatti 8', 'Bugatti 8', 7, 7, '2016-11-30 02:04:09', '2016-11-30 02:04:09'),
 (62, 'Iphone 6', 'Iphone 6', 34538843, '<p>Iphone 6</p>\r\n', 'Iphone 6', 'apple_iphone6_hero_cc003.jpg', 'Iphone 6', 'Iphone 6', 7, 7, '2016-11-30 02:29:11', '2016-11-30 02:29:11'),
-(63, 'Iphone 6S', 'Iphone 6S', 342423423, '<p>Iphone 6S</p>\r\n', 'Iphone 6S', 'apple_iphone6_hero_cc003.jpg', 'Iphone 6', 'Iphone 6S', 7, 7, '2016-11-30 02:35:10', '2016-11-30 02:35:10');
+(63, 'Iphone 6S', 'Iphone 6S', 342423423, '<p>Iphone 6S</p>\r\n', 'Iphone 6S', 'apple_iphone6_hero_cc003.jpg', 'Iphone 6', 'Iphone 6S', 7, 7, '2016-11-30 02:35:10', '2016-11-30 02:35:10'),
+(65, 'Quần Âu Công Sở Nữ', 'Quần Âu Công Sở Nữ', 423423423, '<p>Quần &Acirc;u C&ocirc;ng Sở Nữ</p>\r\n', 'Quần Âu Công Sở Nữ', 'quan-au-nu-cong-so-1.jpg', 'quan-au-nu-cong-so', 'Quần Âu Công Sở Nữ', 7, 1, '2016-12-12 21:51:34', '2016-12-12 21:51:34'),
+(66, 'Quần Âu Nữ Đẹp', 'Quần Âu Nữ Đẹp', 848237, '<p>Quần &Acirc;u Nữ Đẹp</p>\r\n', 'Quần Âu Nữ Đẹp', 'quan-au-nu-dep1.jpg', 'Quần Âu Nữ Đẹp', 'Quần Âu Nữ Đẹp', 7, 1, '2016-12-13 00:16:48', '2016-12-13 00:16:48'),
+(67, 'Quần Âu Nữ Đẹp Xấu', 'Quần Âu Nữ Đẹp Xấu', 848237, '<p>Quần &Acirc;u Nữ Đẹp Xấu</p>\r\n', 'Quần Âu Nữ Đẹp Xấu', 'quan-au-nu-dep2.jpg', 'Quần Âu Nữ Đẹp', 'Quần Âu Nữ Đẹp Xấu', 7, 1, '2016-12-13 00:19:25', '2016-12-13 00:19:25'),
+(68, 'Quần Âu Nữ Đẹp Xấu Đẹp', 'Quần Âu Nữ Đẹp Xấu Đẹp', 34234234, '<p>Quần &Acirc;u Nữ Đẹp Xấu Đẹp</p>\r\n', 'Quần Âu Nữ Đẹp Xấu Đẹp', 'quan-au-nu-dep3.jpg', 'Quần Âu Nữ Đẹp Xấu Đẹp', 'Quần Âu Nữ Đẹp Xấu Đẹp', 7, 1, '2016-12-13 00:20:35', '2016-12-13 00:20:35'),
+(76, 'Quần Nam Công Sở 2', 'Quần Nam Công Sở 2', 3985258, '<p>Quần Nam C&ocirc;ng Sở 2</p>\r\n', 'Quần Nam Công Sở 2', 'quan-nam-cong-so7.jpg', 'Quần Nam Công Sở 2', 'Quần Nam Công Sở 2', 7, 1, '2016-12-13 20:20:58', '2016-12-13 20:20:58'),
+(77, 'Quần Công Sở 3', 'Quần Công Sở 3', 3985248, '<p>Quần C&ocirc;ng Sở 3</p>\r\n', 'Quần Công Sở 3', 'quan-nam-cong-so10.jpg', 'Quần Công Sở 3', 'Quần Công Sở 3', 7, 1, '2016-12-13 20:24:41', '2016-12-13 20:24:41'),
+(78, 'Quần Công Sở 5', 'Quần Công Sở 5', 5725732, '<p>Quần C&ocirc;ng Sở 5</p>\r\n', 'Quần Công Sở 5', 'quan-nam-cong-so19.jpg', 'Quần Công Sở 5', 'Quần Công Sở 5', 7, 1, '2016-12-13 20:31:11', '2016-12-13 20:31:11');
 
 -- --------------------------------------------------------
 
@@ -194,7 +190,9 @@ INSERT INTO `product_images` (`id`, `image`, `product_id`, `created_at`, `update
 (63, 'quat1.jpg', 22, NULL, NULL),
 (64, 'quat2.jpg', 22, NULL, NULL),
 (65, 'quat3.jpg', 22, NULL, NULL),
-(66, 'apple-iphone-6-1.jpg', 62, NULL, NULL);
+(66, 'apple-iphone-6-1.jpg', 62, NULL, NULL),
+(69, 'quan-nam-cong-so8.jpg', 76, NULL, NULL),
+(70, 'quan-nam-cong-so20.jpg', 78, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -221,15 +219,15 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `level`, `remember_t
 (1, 'akkerise', '12345678', 'nguyenthanh.rise.88@gmail.com', 2, NULL, NULL, NULL),
 (3, 'akinhdinh', '$2y$10$p0WVkAoRy1huxARlVmZRyeYf/1NUZH/60RuPfZOeiBsH1jf10fr2a', 'nguyneasda@gmail.com', 2, '1Wv7JtsqZ6jUU9f1vHG8eSLfLhe8tdWEDzAg7I9E', '2016-11-17 04:04:31', '2016-11-17 04:04:31'),
 (5, 'doidepzai', '$2y$10$uDgw5EVnmqNLSXyXYrz9OO3ttfLE6Gv9uON/hLCnoOiY9kYNazs.a', 'quingu@gmail.com', 2, 'sLAjGhbTA6HDtY9K663EohE9u3t1A2dKDt8anyFk', '2016-11-17 04:38:43', '2016-11-17 04:38:43'),
-(7, 'nguyenthanh88', '$2y$10$0eaTDgCHfP01kqYLxfOikOic6GNqvtBQ2crH4J/1hkl.32UdJToCy', 'nguyenthanhabc@gmail.com', 1, '329ZyVsEF3X9iZGmvCLwFP9FpgHUlSDPxqT4Si5bAakWMZEYcLFjN6DoyDjB', '2016-11-17 20:39:01', '2016-11-24 04:03:17'),
+(7, 'nguyenthanh88', '$2y$10$0eaTDgCHfP01kqYLxfOikOic6GNqvtBQ2crH4J/1hkl.32UdJToCy', 'nguyenthanhabc@gmail.com', 1, 'XLCppqgGf44xOpM44nICJsY74AahxA0HEWEL0NFx4cF0GH2Zk7H4WWDdnVJu', '2016-11-17 20:39:01', '2016-12-21 05:46:22'),
 (8, 'nguyenabc', '$2y$10$viMdlHvGEpxxXkbYzZlPeuz6.n6yVSseknkysTpJbIly6Pn6a2iMW', 'nguyentha@ajsd.cc', 3, 'RpUTznJVme89eVW5PNUyYYjpsbfCCCDblxPyQ8Vs', '2016-11-21 01:31:34', '2016-11-21 01:31:34'),
 (9, 'saikodo', '$2y$10$7Ua76dVekOAbgYtjw.5.auDe3PnaPcJ8iyKakhDzJ9PBULNajWoCW', 'saikodo@gmail.com', 2, 'RpUTznJVme89eVW5PNUyYYjpsbfCCCDblxPyQ8Vs', '2016-11-21 01:31:59', '2016-11-24 03:23:46'),
 (10, 'member', '$2y$10$xgVVBQD8.XwcovvFKTeaqe1z0Qk/7zdlJkXW2GYWTytxEjyUiee0G', 'asfdskfk@fsjdjf.com', 3, 'RpUTznJVme89eVW5PNUyYYjpsbfCCCDblxPyQ8Vs', '2016-11-21 01:36:31', '2016-11-21 01:36:31'),
 (11, 'akkesorrybaby', '$2y$10$VUSOv2S3KwFzv.We2Yq1/OrOOOv/YOSxyn9p2lciVnwIgQp4EfM.S', 'akkesorry@gmail.com', 2, 'RpUTznJVme89eVW5PNUyYYjpsbfCCCDblxPyQ8Vs', '2016-11-21 03:34:56', '2016-11-24 04:03:07'),
 (12, 'akkesorry88', '$2y$10$QzSgvaad5nSWJ01yXl.UuOVqndM.BuqlvIVodmJVFYWw2tFh.eYqG', 'nguyenthanh.rise.88@gmail.com', 2, 'RpUTznJVme89eVW5PNUyYYjpsbfCCCDblxPyQ8Vs', '2016-11-21 03:51:11', '2016-11-21 03:51:11'),
 (13, 'withlove', '$2y$10$LJZ/CqbuL4e.mIUiHDY5cOr.mVUjeWQ.1rEnZzu4cr1kFp/imr5qS', 'withlove@gmail.com', 2, 'MhuEKS2nu51Wvtx829bmf4YGXBZW1meVcB2A1epG39f8NVBLrRIyFkwUJT2O', '2016-11-21 20:30:57', '2016-11-22 00:53:06'),
-(16, 'thanhkinh', '$2y$10$ALlg9Sj1mX23IRLKNKRW5e6B3HhVKmbfHWO4cv78QcLAyzUIeQLOa', 'thanhkinh@gmail.com', 3, 'VjyS2STMON2zFP6IEwf099HCI25135Qm1KYMopY0', '2016-11-22 01:03:02', '2016-11-22 01:03:02'),
-(17, 'sorrybaby', '$2y$10$bU1rZAO5fb.FA6x33nQ9zeo5SytMwzTV9rFNkOcpA06QScQaEzJBW', 'sorrybaby@gmail.com', 1, 'VjyS2STMON2zFP6IEwf099HCI25135Qm1KYMopY0', '2016-11-22 03:25:31', '2016-11-24 02:38:59');
+(17, 'sorrybaby', '$2y$10$bU1rZAO5fb.FA6x33nQ9zeo5SytMwzTV9rFNkOcpA06QScQaEzJBW', 'sorrybaby@gmail.com', 1, 'VjyS2STMON2zFP6IEwf099HCI25135Qm1KYMopY0', '2016-11-22 03:25:31', '2016-11-24 02:38:59'),
+(18, 'thehientk', '$2y$10$khnvRAAxZoP4KNpaszJcOuswscst..2FFseyLqVax0xFYBRtmeCP2', 'thehientk@gmail.com', 2, 'UnCNIRRXCCYigWTgzy4uceD8cXZqVhhCe4inFhzi', '2016-12-21 05:45:47', '2016-12-21 05:45:47');
 
 --
 -- Indexes for dumped tables
@@ -296,17 +294,17 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 --
 -- AUTO_INCREMENT for table `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- Constraints for dumped tables
 --
