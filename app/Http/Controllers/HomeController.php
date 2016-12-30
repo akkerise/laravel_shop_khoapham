3cf3f04cbba99fd5b64bb5b09502a5276032ec75
AkKeRise <akkerise@gmail.com>
2016-12-31 00:44:05 +0700

    Test id

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -159,7 +159,7 @@ class HomeController extends Controller {
 			array_push($id_need, $key->id);
 		}
 		// $products = Product::select()->orderBy('id', 'DESC')->offset([$id_need[0], $id_need[1], $id_need[2], $id_need[3]])->limit(4)->get()->toArray();
-		$products = Product::select()->orderBy('id', 'DESC')->limit(4)->get()->toArray();
+		$products = Product::select()->orderBy('id', 'DESC')->offset(4)->limit(4)->get()->toArray();
 		return $products;
 	}
 
