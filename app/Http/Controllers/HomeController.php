bce28865ac89adb9f9db014e1cf1a363cbe7a643
AkKeRise <akkerise@gmail.com>
2016-12-31 00:56:01 +0700

    Test id

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -152,15 +152,15 @@ class HomeController extends Controller {
 		return route('home');
 	}
 
-	public function ajax($limit = 0) {
+	public function ajax($qty = 0) {
 		$id_skip_product = Product::select('id')->orderBy('id', 'DESC')->take(4)->get();
 		$id_need         = array();
 		foreach ($id_skip_product as $key) {
 			array_push($id_need, $key->id);
 		}
 		// $products = Product::select()->orderBy('id', 'DESC')->offset([$id_need[0], $id_need[1], $id_need[2], $id_need[3]])->limit(4)->get()->toArray();
-		$limit += 4;
-		$products = Product::select()->orderBy('id', 'DESC')->offset($limit)->take(4)->get()->toArray();
+		$qty += 4;
+		$products = Product::select()->orderBy('id', 'DESC')->offset($qty)->take(4)->get()->toArray();
 		return $products;
 	}
 
