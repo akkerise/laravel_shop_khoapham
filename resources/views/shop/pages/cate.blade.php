a384662c65746008f83db59caf7fff510b55cae0
AkKeRise <akkerise@gmail.com>
2017-01-03 23:21:26 +0700

    Add listProducts

--- resources/views/shop/pages/cate.blade.php
+++ resources/views/shop/pages/cate.blade.php
@@ -37,11 +37,11 @@ AkKe Category
             <ul class="bestseller">
             @if(isset($newest_products))
               @foreach($newest_products as $np)
-              {{-- {{ $cate_name = DB::table('cates')->select('name')->where('id',$np->cate_id)->get() }} --}}
+              {{ $cate_name = DB::table('cates')->select('name')->where('id',$np->cate_id)->get() }}
               <li>
                 <img width="50" height="50" src="{{ asset('/image/'.$np->image) }}" alt="product" title="product">
                 <a class="productname" href="{{ route('productDetail',[$np->id,$np->name]) }}">{{ $np->name }}</a>
-                {{-- <span class="procategory">{{ $cate_name[0]->name }}</span> --}}
+                <span class="procategory">{{ $cate_name[0]->name }}</span>
                 <span class="price">{{ number_format($np->price,0,',','.') }} VNĐ</span>
               </li>
               @endforeach
