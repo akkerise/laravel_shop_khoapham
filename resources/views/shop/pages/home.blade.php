58eb3d05996e79878f7936826baaa9084a075546
AkKeRise <akkerise@gmail.com>
2017-03-03 03:13:43 +0700

    Test ajax for button LoadMore of home.blade.php

--- resources/views/shop/pages/home.blade.php
+++ resources/views/shop/pages/home.blade.php
@@ -81,7 +81,7 @@ function loadMore(){
       url: "https://limitless-peak-35722.herokuapp.com/ajax-loadmore?page=" + page,
       data: {token: token , page: page},
       success: function (data) {
-          console.log(data[0]);
+          console.log(data);
 
       }
   });
