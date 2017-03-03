d4b7bf29020c7e7d4372001d05d0a2522f8c08e2
AkKeRise <akkerise@gmail.com>
2017-03-03 17:04:35 +0700

    Test ajax for button LoadMore of home.blade.php

--- resources/views/shop/master.blade.php
+++ resources/views/shop/master.blade.php
@@ -62,8 +62,8 @@
     ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="{{ secure_url('shop.js.jquery.js') }}"></script>
-  <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+  {{--<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>--}}
+  {{--<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>--}}
 <script src="{{ secure_url('/shop/js/bootstrap.js') }}"></script>
 <script src="{{ secure_url('/shop/js/respond.min.js') }}"></script>
 <script src="{{ secure_url('/shop/js/application.js') }}"></script>
