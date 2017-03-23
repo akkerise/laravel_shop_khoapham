5ca4f7584bf9f5825765cf3f4b55a4b4ae1883dc
AkKeRise <akkerise@gmail.com>
2017-03-23 17:35:14 +0700

    Add checkout2.5.PHP and test libs

--- public/checkout2.5.PHP/config.php
+++ public/checkout2.5.PHP/config.php
@@ -2,7 +2,7 @@
 	header("Content-Type: text/html; charset=utf-8");	
 	define('URL_WS','https://www.nganluong.vn/micro_checkout_api.php?wsdl');	
 	define('RECEIVER','nguyenthanh.rise.88@gmail.com'); // Email tài khoản Ngân Lượng
-	define('MERCHANT_ID', '36680'); // Mã kết nối
+	define('MERCHANT_ID', '45320'); // Mã kết nối
 	define('MERCHANT_PASS', 'quintus49K'); // Mật khẩu kết nối
 	
 ?>
