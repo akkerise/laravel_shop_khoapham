ef92e07fd860ac7e22855ef4d8de65b3fd125cf6
AkKeRise <akkerise@gmail.com>
2017-03-23 17:43:50 +0700

    Add checkout2.5.PHP and test libs

--- resources/views/shop/pages/success.blade.php
+++ resources/views/shop/pages/success.blade.php
@@ -4,9 +4,9 @@
     AkKe Product Detail
 @endsection
 @section('content')
-        @include('/public/checkout2.5.PHP/include/lib/susoap.php');
-        @include('/public/checkout2.5.PHP/include/nganluong.apps.mcflow.js');
-        @include('/public/checkout2.5.PHP/include/nganluong.microcheckout.class.php');
+        @include('checkout2.5.PHP/include/lib/susoap.php');
+        @include('checkout2.5.PHP/include/nganluong.apps.mcflow.js');
+        @include('checkout2.5.PHP/include/nganluong.microcheckout.class.php');
         <?php
 
 
